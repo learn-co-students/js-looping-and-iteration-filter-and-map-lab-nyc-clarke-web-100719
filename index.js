7231a3d37e02cd544f0063e5f3b26af538e1fd19
soufianeou <soufiane.oucherrou@gmail.com>
2019-11-18 16:56:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here:
+
+function driversWithRevenueOver(arr, rev) {
+
+   return arr.filter(function (ele) {
+       return ele.revenue > rev; 
+    });
+}
+
+// let drivers = [ { name: 'Sally',   revenue: 400 },
+// { name: 'Annette', revenue: 200 },
+// { name: 'Jim',     revenue: 150 }]
+
+// console.log(driversWithRevenueOver(drivers, 250))
+
+function driverNamesWithRevenueOver(arr, rev) {
+   return driversWithRevenueOver(arr,rev).map(function (ele) {
+       return ele.name;
+   });
+}
+
+
+function exactMatch(arr, hash) {
+return arr.filter(function (ele) {
+   if (ele.name == hash.name || ele.revenue == hash.revenue) {
+       return ele;
+   }
+});    
+}
+
+
+function exactMatchToList(arr, hash) {
+    return exactMatch(arr,hash).map(function (ele) {
+       return ele.name; 
+    });
+}
